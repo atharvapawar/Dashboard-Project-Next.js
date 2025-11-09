2f06be5b2b375f14ab08258beedb31d71608be7f
Atharv <atharvapawar34s@gmail.com>
2025-11-09 12:11:17 +0530

    setting up task page...

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const page = () => {
+
+  const metrics = [
+    {
+      title: 'Total Sales',
+      value: '$12,00,00',
+    }
+  ]
+
   return <div>Analytics Page</div>;
 };
 
